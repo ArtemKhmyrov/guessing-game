027730a202d4fd07cfbcd052154f83bd3a1e3467
Хмыров А.М <a.hmyrov-saratov@mail.ru>
2023-09-26 05:11:20 +0400

    finish version

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min;
+        this.max;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.res = Math.round((this.max - this.min) / 2) + this.min;
+        return this.res;
     }
 
     lower() {
-
+        this.max = this.res;
     }
 
     greater() {
-
+        this.min = this.res;
     }
 }
 
